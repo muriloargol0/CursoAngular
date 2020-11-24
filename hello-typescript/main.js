63ef0fb0b84dfd194e30467dd8ad99926da9b800
muriloargol0 <murilo.argolo@live.com>
2020-11-24 16:34:06 -0300

    Curso Angular

---
+++ hello-typescript/main.js
@@ -0,0 +1,3 @@
+var minhaVar = 'minha variável';
+var numeros = [1, 2, 3];
+numeros.map(function (valor) { return valor * 2; });
---
+++ hello-typescript/main.ts
@@ -0,0 +1,5 @@
+var minhaVar = 'minha variável';
+
+var numeros = [1, 2, 3]
+
+numeros.map(valor => valor * 2);
